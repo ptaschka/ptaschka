4533f1849255e45ebe925ce3fc9774ba1b982378
ptaschka <101614310+ptaschka@users.noreply.github.com>
2022-05-24 23:54:37 +0700

    Add files via upload

--- 17.9.1.py
+++ 17.9.1.py
@@ -1,4 +1,4 @@
-array = input("Введите числа от 1 до 10 через пробел: ").split()
+array = input("Введите числа от 1 до 100 через пробел: ").split()
 L = list(map(int, array))
 for i in range(len(L)):
     idx_min = i
@@ -8,17 +8,18 @@ for i in range(len(L)):
     if i != idx_min:
         L[i], L[idx_min] = L[idx_min], L[i]
 print(L)
-print(len(L))
 element = int(input("Введите число: "))
-def binary_search(L, element, left, right):
-    if left > right:
-        return False
-    middle = (right + left) // 2
-    if L[middle] == element:
-        return middle
-    elif element < L[middle]:
-        return binary_search(L, element, left, middle - 1)
-    else:
-        return binary_search(L, element, middle + 1, right)
-
-print(binary_search(L, element, 0, len(L)))
+if max(L) < element < min(L):
+    print("Число не соответствует заданному условию")
+else:
+    def binary_search(L, element, left, right):
+        if left > right:
+            return False
+        middle = (right + left) // 2
+        if L[middle] == element:
+            return middle
+        elif element < L[middle]:
+            return binary_search(L, element, left, middle - 1)
+        else:
+            return binary_search(L, element, middle + 1, right)
+    print(binary_search(L, element-1, 0, len(L)))
